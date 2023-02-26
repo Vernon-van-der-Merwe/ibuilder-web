63e841e0a9be8f87c05183ff0ab771c8bd5d2508
Vernon van der Merwe <vernon.developer@gmail.com>
2023-02-26 18:08:22 +0200

    chore: Add firebase

---
+++ config/firebaseConfig.js
@@ -0,0 +1,13 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyArG9OF-g0VqOepE8eVO_s8_AwjbxvEYN4",
+  authDomain: "ibuilder-561ae.firebaseapp.com",
+  projectId: "ibuilder-561ae",
+  storageBucket: "ibuilder-561ae.appspot.com",
+  messagingSenderId: "369325995638",
+  appId: "1:369325995638:web:668fe1661fcace3e87d157"
+};
+
+const app = initializeApp(firebaseConfig);
